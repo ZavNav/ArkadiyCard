dfdefcf2a5921ee86d596fc84f7c9584d69dc1aa
ZavNav <arka146@mail.ru>
2022-03-16 15:55:58 +0300

    Initial Commit

---
+++ ArkadiyCard/ArkadiyCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArkadiyCardApp.swift
+//  ArkadiyCard
+//
+//  Created by Аркадий Рожков on 16.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct ArkadiyCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
